6d80814b652f793eed7c69a6005bb9b241934e20
Christos Boukouvalas <christos.boukouvalas.90@gmail.com>
2022-11-30 16:07:43 +0200

    Install Tailwind CSS and Daisy UI

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="dark">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
